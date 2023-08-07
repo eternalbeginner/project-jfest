9eaf287e9f3157c2ae60bf3da120ef0ca973e026
eternalbeginner <eternal.beginner.dev@gmail.com>
2023-08-07 17:10:29 +0800

    fix bug

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,1 +1,4 @@
-export { default as Footer } from './footer';
+export { default as Footer } from './Footer';
+export { default as FooterBio } from './FooterBio';
+export { default as FooterContacts } from './FooterContacts';
+export { default as FooterSocials } from './FooterSocials';
